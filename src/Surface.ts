3c23f8c9c1a3362743728a62269da937090795fd
Legokichi Duckscallion <legokichi@duxca.com>
2015-05-01 00:01:38 +0900

    write

---
+++ src/Surface.ts
@@ -0,0 +1,216 @@
+/// <reference path="SurfaceRender.ts"/>
+/// <reference path="SurfaceUtil.ts"/>
+
+module cuttlebone {
+  export class Surface {
+    element: HTMLCanvasElement;
+    scopeId: number;
+    surfaceId: number;
+    surfaceName: string;
+    surfaces: { [key: string]: SurfaceDefinition; };
+    isRegionVisible: boolean;
+    baseSurface: HTMLCanvasElement;
+    regions: { [key: string]: SurfaceRegion };
+    animations: { [key: string]: SurfaceAnimation; };
+    bufferCanvas: HTMLCanvasElement;
+    stopFlags: { [key: string]: boolean; };
+    layers: { [is: number]: SurfaceAnimationPattern; };
+    destructed: boolean;
+    talkCount: number;
+    talkCounts: { [key: string]: number };
+    isPointerEventsShimed: boolean;
+    lastEventType: string;
+    constructor(
+      canvas: HTMLCanvasElement,
+      scopeId: number,
+      surfaceId: number,
+      surfaceName: string,
+      surfaces: { [key: string]: SurfaceDefinition; }
+      ) {
+      this.element = canvas;
+      this.scopeId = scopeId;
+      this.surfaceId = surfaceId;
+      this.surfaceName = surfaceName;
+      this.surfaces = surfaces;
+      var srf = this.surfaces[surfaceName];
+      if (!srf) throw new Error(surfaceName + " is not found in surfaces");
+      if (!srf.baseSurface) console.warn("baseSurface is not found", this);
+      this.baseSurface = srf.baseSurface;
+      this.regions = srf.regions || {};
+      this.animations = srf.animations || {};
+      this.bufferCanvas = SurfaceUtil.copy(this.baseSurface);
+      this.stopFlags = {};
+      this.layers = {};
+      this.destructed = false;
+      this.talkCount = 0;
+      this.talkCounts = {};
+      this.isPointerEventsShimed = false;
+      this.isRegionVisible = false;
+      this.lastEventType = "";
+      this.element.addEventListener("contextmenu", (ev) => this.processMouseEvent(ev, "mouseclick", (ev) => this.element.dispatchEvent(ev)));
+      this.element.addEventListener("click", (ev) => this.processMouseEvent(ev, "mouseclick", (ev) => this.element.dispatchEvent(ev)));
+      this.element.addEventListener("dblclick", (ev) => this.processMouseEvent(ev, "mousedblclick", (ev) => this.element.dispatchEvent(ev)));
+      this.element.addEventListener("mousedown", (ev) => this.processMouseEvent(ev, "mousedown", (ev) => this.element.dispatchEvent(ev)));
+      this.element.addEventListener("mousemove", (ev) => this.processMouseEvent(ev, "mousemove", (ev) => this.element.dispatchEvent(ev)));
+      this.element.addEventListener("mouseup", (ev) => this.processMouseEvent(ev, "mouseup", (ev) => this.element.dispatchEvent(ev)));
+      var tid = 0;
+      var touchCount = 0;
+      var touchStartTime = 0;
+      this.element.addEventListener("touchmove", (ev) => this.processMouseEvent(ev, "mousemove", (ev) => this.element.dispatchEvent(ev)));
+      this.element.addEventListener("touchend", (ev) => {
+        this.processMouseEvent(ev, "mouseup", (ev) => this.element.dispatchEvent(ev));
+        this.processMouseEvent(ev, "mouseclick", (ev) => this.element.dispatchEvent(ev));
+        if (Date.now() - touchStartTime < 500 && touchCount % 2 === 0) {
+          this.processMouseEvent(ev, "mousedblclick", (ev) => this.element.dispatchEvent(ev));
+        }
+      });
+      this.element.addEventListener("touchstart", (ev) => {
+        touchCount++;
+        touchStartTime = Date.now();
+        this.processMouseEvent(ev, "mousedown", (ev) => this.element.dispatchEvent(ev));
+        clearTimeout(tid);
+        tid = setTimeout((() => touchCount = 0), 500);
+      });
+      Object.keys(this.animations).forEach((name) => {
+        var {is: animationId, interval, patterns} = this.animations[name];
+        interval = interval || "";
+        var tmp = interval.split(",");
+        interval = tmp[0];
+        var n = Number(tmp.slice(1).join(","));
+        switch (interval) {
+          case "sometimes": SurfaceUtil.random(((callback) => { if (!this.destructed && !this.stopFlags[animationId]) { this.play(animationId, callback); } }), 2); break;
+          case "rarely": SurfaceUtil.random(((callback) => { if (!this.destructed && !this.stopFlags[animationId]) { this.play(animationId, callback); } }), 4); break;
+          case "random": SurfaceUtil.random(((callback) => { if (!this.destructed && !this.stopFlags[animationId]) { this.play(animationId, callback); } }), n); break;
+          case "periodic": SurfaceUtil.periodic(((callback) => { if (!this.destructed && !this.stopFlags[animationId]) { this.play(animationId, callback); } }), n); break;
+          case "always": SurfaceUtil.always(((callback) => { if (!this.destructed && !this.stopFlags[animationId]) { this.play(animationId, callback); } })); break;
+          case "runonce": this.play(animationId); break;
+          case "never": break;
+          case "bind": break;
+          case "yen-e": break;
+          case "talk": this.talkCounts[name] = n; break;
+          default:
+            if (/^bind(?:\+(\d+))/.test(interval)) {
+              // 着せ替えなので何もしない
+            } else {
+              console.warn(this.animations[name]);
+            }
+        }
+      });
+      this.render();
+    }
+    destructor(): void {
+      var srfRdr = new SurfaceRender(this.element);
+      srfRdr.clear();
+      //$(@element).off() # g.c.
+      this.destructed = true;
+      this.layers = {};
+    }
+    render(): void { }
+    talk(): void { }
+    yenE(): void { }
+    play(animationId: number, callback?: () => void): void { }
+    stop(animationId: number): void {
+      this.stopFlags[animationId] = true;
+    }
+    bind(animationId: number): void {
+      var hit = Object.keys(this.animations).filter((name)=> this.animations[name].is === animationId)[0];
+      if (!hit) return;
+      var anim = this.animations[hit];
+      if (anim.patterns.length === 0 ) return;
+      var interval = anim.interval;
+      var pattern = anim.patterns[anim.patterns.length-1];
+      this.layers[anim.is] = pattern;
+      this.render();
+      if(/^bind(?:\+(\d+))/.test(interval)){
+        var animIds = interval.split("+").slice(1);
+        animIds.forEach((animId)=> this.play(Number(animId)));
+      }
+    }
+    unbind(animationId: number): void {
+      delete this.layers[animationId];
+    }
+    processMouseEvent(ev: any, eventName: string, callback: (ev: any) => void): void {
+      
+    }
+  }
+}
+
+interface SurfacesTxt {
+  charset: string;
+  descript: {
+    version: number;
+    maxwidth: number;
+    "collision-sort": string;
+    "animation-sort": string;
+  };
+  surfaces: { [key: string]: SurfaceDefinition; };
+  aliases: { sakura: { [name: string]: number[]; }; };
+  regions: {
+    sakura: {
+      bust: {
+        tooltip: string;
+        cursor: {
+          mouseup: string;
+          mousedown: string;
+        };
+      };
+    };
+  };
+}
+
+interface SurfaceDefinition {
+  is: number;
+  characters: { sakura: string; };
+  points: {
+    centerx: number;
+    centery: number;
+    kinoko: { centerx: number; centery: number; },
+    basepos: { x: number; y: number; };
+  };
+  balloons: {
+    sakura: { offsetx: number; offsety: number; };
+    offsetx: number;
+    offsety: number;
+  };
+  regions: { [key: string]: SurfaceRegion; };
+  animations: { [key: string]: SurfaceAnimation; };
+  baseSurface: HTMLCanvasElement;
+  base: string[];
+}
+
+interface SurfaceLayerObject {
+  is: number;
+  canvas: HTMLCanvasElement;
+  type: string;
+  x: number;
+  y: number;
+}
+
+interface SurfaceAnimation {
+  is: number;
+  interval: string;
+  option: string;
+  patterns: SurfaceAnimationPattern[];
+}
+
+interface SurfaceAnimationPattern {
+  type: string;
+  surface: number;
+  wait: string;
+  x: number;
+  y: number;
+}
+
+interface SurfaceRegion {
+  is: number;
+  name: string;
+  type: string;
+  left: number;
+  top: number;
+  right: number;
+  bottom: number;
+  radius: number;
+  center_x: number;
+  center_y: number;
+  coordinates: { x: number; y: number; }[];
+}
---
+++ src/SurfaceUtil.ts
@@ -0,0 +1,89 @@
+/// <reference path="../typings/bluebird/bluebird.d.ts" />
+
+module cuttlebone {
+  export module SurfaceUtil {
+    export function choice<T>(arr: T[]): T {
+      return arr[Math.round(Math.random()*(arr.length-1))];
+    }
+    export function copy(cnv: HTMLCanvasElement|HTMLImageElement): HTMLCanvasElement {
+      var copy = document.createElement("canvas");
+      var ctx = <CanvasRenderingContext2D>copy.getContext("2d");
+      copy.width = cnv.width;
+      copy.height = cnv.height;
+      ctx.drawImage(<HTMLCanvasElement>cnv, 0, 0); // type hack
+      return copy;
+    }
+    export function fetchImage(url: string): Promise<HTMLImageElement> {
+      var img = new Image;
+      img.src = url;
+      return new Promise<HTMLImageElement>((resolve, reject)=>{
+        img.addEventListener("load", function() {
+          resolve(Promise.resolve(img)); // type hack
+        });
+        img.addEventListener("error", function(ev) {
+          reject(ev.error);
+        });
+      });
+    }
+    export function random(callback: (callback: () => void) => void, probability: number): void {
+      var ms = 1;
+      while (Math.round(Math.random() * 1000) > 1000 / probability) {
+        ms++;
+      }
+      setTimeout((() =>
+        callback(() => random(callback, probability)) 
+      ), ms * 1000);
+    }
+    export function periodic(callback: (callback: () => void) => void, sec: number): void {
+      setTimeout((() =>
+        callback(()=>
+          periodic(callback, sec)
+        )
+      ), sec * 1000);
+    }
+    export function always(  callback: (callback: () => void) => void): void {
+      callback(() => always(callback) );
+    }
+    export function isHit(cnv: HTMLCanvasElement, x: number, y: number ): boolean {
+      var ctx = <CanvasRenderingContext2D>cnv.getContext("2d");
+      var imgdata = ctx.getImageData(0, 0, x + 1, y + 1);
+      var data = imgdata.data;
+      return data[data.length - 1] !== 0;
+    }
+    export function offset(element: HTMLElement): {left: number, top: number, width: number, height: number} {
+      var obj = element.getBoundingClientRect();
+      return {
+        left: obj.left + window.pageXOffset,
+        top: obj.top + window.pageYOffset,
+        width: Math.round(obj.width),
+        height: Math.round(obj.height)
+      };
+    }	
+    export function elementFromPointWithout (element: HTMLElement, pageX: number, pageY: number): HTMLElement {
+      var tmp = element.style.display;
+      element.style.display = "none";
+      // elementを非表示にして直下の要素を調べる
+      var elm = document.elementFromPoint(pageX, pageY);
+      // 直下の要素がcanvasなら透明かどうか調べる
+      // todo: cuttlebone管理下の要素かどうかの判定必要
+      if (elm instanceof HTMLCanvasElement) {
+        var {top, left} = offset(elm);
+        // 不透明ならヒット
+        if (isHit(elm, pageX - left, pageY - top)) {
+          element.style.display = tmp;
+          return elm;
+        }
+      }
+      // elementの非表示のままさらに下の要素を調べにいく
+      if (elm instanceof HTMLElement) {
+        var _elm = elementFromPointWithout(elm, pageX, pageY)
+        element.style.display = tmp;
+        return _elm;
+      }
+      // 解決できなかった！ザンネン!
+      console.warn(elm);
+      element.style.display = tmp;
+      return null;
+    }
+  }
+}
