22385cc11ce472f0f9d47ed1a1dc96b7814bdc78
Legokichi Duckscallion <legokichi@duxca.com>
2015-05-18 21:13:09 +0900

    update

--- src/BlobWorker.ts
+++ src/BlobWorker.ts
@@ -4,8 +4,9 @@ module cuttlebone {
   function serverScript(): ()=>void{
     return (()=>{
       var _self = <any>self; // type hack
-      var handlers = {};
-      self.addEventListener("message", ({data: [id, event, data]})=>{
+      var handlers:{[key:string]: (data:any, reply:(arg:any[], trf:ArrayBuffer[])=>void)=>void} = {};
+      self.addEventListener("message", (ev)=>{
+        var {data:[id, event, data]} = <{data:[number, string, any]}>ev;
         function reply(args:any[], transferable:ArrayBuffer[]): void {
           _self.postMessage([id, args], transferable);
         }
@@ -49,21 +50,23 @@ module cuttlebone {
       this.requestId = 0
       this.callbacks = {}
     }
+
     request(event:string, callback:()=>void): void;
     request(event:string, tuple:[any, ArrayBuffer[]], callback:()=>void): void;
-    request(event:string, tuple:any, callback?:()=>void): void{
+    request(event:string, tuple:(()=>void)|[any, ArrayBuffer[]], callback?:()=>void): void{
       if(tuple instanceof Function){
-        callback = <()=>void>tuple;
+        callback = tuple;
         var data = <any>null;
         var transferable = <ArrayBuffer[]>[];
       }else if(tuple instanceof Array && tuple[1] instanceof Array){
-        var data = <any>tuple[0];
+        var data = tuple[0];
         var transferable = <ArrayBuffer[]>tuple[1];
       }else throw new Error("BlobWorker#request > TypeError");
       var id = this.requestId++;
       this.callbacks[id] = callback;
       this.worker.postMessage([id, event, data], transferable);
     }
+
     terminate(){
       this.worker.terminate();
       URL.revokeObjectURL(this.url);
